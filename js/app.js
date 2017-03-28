c175e275839bb9efb5a7ec5cee19d6ef6f1b86f2
Nathaniel-j-s <nathaniel.j.mith@gmail.com>
2017-03-28 16:00:58 -0600

    more skeleton work

---
+++ js/app.js
@@ -0,0 +1,38 @@
+angular.module('myApp', ['ui-router'])
+  .config(function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.when('', '/');
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+      .state('welcome', {
+        url: '/',
+        templateUrl: './welcome.html',
+        controller: 'myController'
+      })
+      .state('login', {
+        url: '/login',
+        templateUrl: '../login.html',
+        controller: 'myController'
+      })
+      .state('newRoom', {
+        url: '/new-room',
+        templateUrl: './new-room.html',
+        controller: 'myController'
+      })
+      .state('roomList', {
+        url: '/room-list',
+        templateUrl: '../room-list.html',
+        controller: 'myController'
+      })
+      .state('calendar', {
+        url: '/calendar',
+        templateUrl: '../calendar.html',
+        controller: 'myController'
+      })
+      .state('booking', {
+        url: '/booking',
+        templateUrl: '../booking.html',
+        controller: 'myController'
+      })
+
+  })
